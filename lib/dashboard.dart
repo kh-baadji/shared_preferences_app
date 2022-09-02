1315b2c5693b1e495515b7d4c1fad253e8e93056
BAADJI Khaled <kh.baadji@gmail.com>
2022-09-02 20:21:37 +0100

    shared preferences package testing

---
+++ lib/dashboard.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  String token = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getCred();
+  }
+
+  void getCred() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      token = pref.getString('login')!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                const Text('Welcome User'),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text('Your Token : ${token}')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
